686f6490eabbd12df279e2d7fbfd43258ad28327
Maria Rumyantseva <manechka96@list.ru>
2023-02-20 21:05:10 +0300

    feat: add GuessingGame class

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,23 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+    }
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        this.guessNumber = this.min + Math.round((this.max - this.min) / 2);
+        return this.guessNumber;
     }
 
     lower() {
-
+        this.max = this.guessNumber;
     }
 
     greater() {
-
+        this.min = this.guessNumber;
     }
 }
 
